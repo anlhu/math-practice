c1901a42dafa9dbb07a98e1e4a478935d9dbaa25
Andrew Hu <anlhu@ucsc.edu>
2024-03-26 18:07:25 -0700

    fix git, push new refactor

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-import tkinter as tk
-from screens import MenuPage, GamePage
-
-class App(tk.Tk):
-	 
-	def __init__(self): 
-		tk.Tk.__init__(self)
-		 
-		container = tk.Frame(self)  
-		container.pack(side = "top", fill = "both", expand = True) 
-		container.grid_rowconfigure(0, weight = 1)
-		container.grid_columnconfigure(0, weight = 1)
-
-		self.frames = {}
-
-		for pageClass in (MenuPage, GamePage):
-			frame = pageClass(container, self)
-			self.frames[pageClass] = frame 
-			frame.grid(row = 0, column = 0, sticky ="nsew")
-  
-		self.show_frame(MenuPage)
-  
-	def show_frame(self, cont):
-		frame = self.frames[cont]
-		frame.tkraise()
+from app import App
 
 app = App()
 app.mainloop()
